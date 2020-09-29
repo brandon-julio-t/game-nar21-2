7d271038bcdfdcda06bfb1e30eacef2822324440
Brandon Julio Thenaro <brandon.julio.t@icloud.com>
2020-09-30 04:00:23 +0700

    Some redesign

---
+++ src/components/DialogGameInstruction.vue
@@ -0,0 +1,114 @@
+<template>
+  <div
+    class="flex justify-center items-center mt-16 p-16 rounded bg-black text-white"
+  >
+    <div>
+      <h1 class="text-xl font-bold mb-4 flex items-center">
+        <svg
+          width="1em"
+          height="1em"
+          viewBox="0 0 16 16"
+          class="bi bi-question-circle"
+          fill="currentColor"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            fill-rule="evenodd"
+            d="M8 15A7 7 0 1 0 8 1a7 7 0 0 0 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"
+          />
+          <path
+            d="M5.255 5.786a.237.237 0 0 0 .241.247h.825c.138 0 .248-.113.266-.25.09-.656.54-1.134 1.342-1.134.686 0 1.314.343 1.314 1.168 0 .635-.374.927-.965 1.371-.673.489-1.206 1.06-1.168 1.987l.003.217a.25.25 0 0 0 .25.246h.811a.25.25 0 0 0 .25-.25v-.105c0-.718.273-.927 1.01-1.486.609-.463 1.244-.977 1.244-2.056 0-1.511-1.276-2.241-2.673-2.241-1.267 0-2.655.59-2.75 2.286zm1.557 5.763c0 .533.425.927 1.01.927.609 0 1.028-.394 1.028-.927 0-.552-.42-.94-1.029-.94-.584 0-1.009.388-1.009.94z"
+          />
+        </svg>
+
+        <span class="ml-2">Instructions</span>
+      </h1>
+
+      <div class="grid grid-cols-2 gap-8">
+        <section>
+          <h2 class="text-lg flex items-center">
+            <svg
+              width="1em"
+              height="1em"
+              viewBox="0 0 16 16"
+              class="bi bi-keyboard-fill"
+              fill="currentColor"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M0 6a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v5a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V6zm13 .25a.25.25 0 0 1 .25-.25h.5a.25.25 0 0 1 .25.25v.5a.25.25 0 0 1-.25.25h-.5a.25.25 0 0 1-.25-.25v-.5zM2.25 8a.25.25 0 0 0-.25.25v.5c0 .138.112.25.25.25h.5A.25.25 0 0 0 3 8.75v-.5A.25.25 0 0 0 2.75 8h-.5zM4 8.25A.25.25 0 0 1 4.25 8h.5a.25.25 0 0 1 .25.25v.5a.25.25 0 0 1-.25.25h-.5A.25.25 0 0 1 4 8.75v-.5zM6.25 8a.25.25 0 0 0-.25.25v.5c0 .138.112.25.25.25h.5A.25.25 0 0 0 7 8.75v-.5A.25.25 0 0 0 6.75 8h-.5zM8 8.25A.25.25 0 0 1 8.25 8h.5a.25.25 0 0 1 .25.25v.5a.25.25 0 0 1-.25.25h-.5A.25.25 0 0 1 8 8.75v-.5zM13.25 8a.25.25 0 0 0-.25.25v.5c0 .138.112.25.25.25h.5a.25.25 0 0 0 .25-.25v-.5a.25.25 0 0 0-.25-.25h-.5zm0 2a.25.25 0 0 0-.25.25v.5c0 .138.112.25.25.25h.5a.25.25 0 0 0 .25-.25v-.5a.25.25 0 0 0-.25-.25h-.5zm-3-2a.25.25 0 0 0-.25.25v.5c0 .138.112.25.25.25h1.5a.25.25 0 0 0 .25-.25v-.5a.25.25 0 0 0-.25-.25h-1.5zm.75 2.25a.25.25 0 0 1 .25-.25h.5a.25.25 0 0 1 .25.25v.5a.25.25 0 0 1-.25.25h-.5a.25.25 0 0 1-.25-.25v-.5zM11.25 6a.25.25 0 0 0-.25.25v.5c0 .138.112.25.25.25h.5a.25.25 0 0 0 .25-.25v-.5a.25.25 0 0 0-.25-.25h-.5zM9 6.25A.25.25 0 0 1 9.25 6h.5a.25.25 0 0 1 .25.25v.5a.25.25 0 0 1-.25.25h-.5A.25.25 0 0 1 9 6.75v-.5zM7.25 6a.25.25 0 0 0-.25.25v.5c0 .138.112.25.25.25h.5A.25.25 0 0 0 8 6.75v-.5A.25.25 0 0 0 7.75 6h-.5zM5 6.25A.25.25 0 0 1 5.25 6h.5a.25.25 0 0 1 .25.25v.5a.25.25 0 0 1-.25.25h-.5A.25.25 0 0 1 5 6.75v-.5zM2.25 6a.25.25 0 0 0-.25.25v.5c0 .138.112.25.25.25h1.5A.25.25 0 0 0 4 6.75v-.5A.25.25 0 0 0 3.75 6h-1.5zM2 10.25a.25.25 0 0 1 .25-.25h.5a.25.25 0 0 1 .25.25v.5a.25.25 0 0 1-.25.25h-.5a.25.25 0 0 1-.25-.25v-.5zM4.25 10a.25.25 0 0 0-.25.25v.5c0 .138.112.25.25.25h5.5a.25.25 0 0 0 .25-.25v-.5a.25.25 0 0 0-.25-.25h-5.5z"
+              />
+            </svg>
+
+            <span class="ml-2">Keyboard</span>
+          </h2>
+          <ul class="list-disc">
+            <li>
+              <kbd>w</kbd>
+              <kbd>a</kbd>
+              <kbd>s</kbd>
+              <kbd>d</kbd>
+              /
+              <kbd>↑</kbd>
+              <kbd>←</kbd>
+              <kbd>↓</kbd>
+              <kbd>→</kbd>
+              move
+            </li>
+            <li><kbd>Shift</kbd> slow down</li>
+            <li><kbd>Space</kbd> shoot</li>
+          </ul>
+        </section>
+
+        <section>
+          <h2 class="text-lg flex items-center">
+            <svg
+              width="1em"
+              height="1em"
+              viewBox="0 0 16 16"
+              class="bi bi-mouse"
+              fill="currentColor"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M8 3a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 3zm4 8V5a4 4 0 0 0-8 0v6a4 4 0 0 0 8 0zM8 0a5 5 0 0 0-5 5v6a5 5 0 0 0 10 0V5a5 5 0 0 0-5-5z"
+              />
+            </svg>
+
+            <span class="ml-2">Mouse</span>
+          </h2>
+          <ul class="list-disc">
+            <li>Mouse move: move</li>
+            <li>Left click: shoot</li>
+          </ul>
+        </section>
+      </div>
+
+      <h1 class="text-xl font-bold my-4 flex items-center">
+        <svg
+          width="1em"
+          height="1em"
+          viewBox="0 0 16 16"
+          class="bi bi-exclamation-circle"
+          fill="currentColor"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            fill-rule="evenodd"
+            d="M8 15A7 7 0 1 0 8 1a7 7 0 0 0 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"
+          />
+          <path
+            d="M7.002 11a1 1 0 1 1 2 0 1 1 0 0 1-2 0zM7.1 4.995a.905.905 0 1 1 1.8 0l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 4.995z"
+          />
+        </svg>
+
+        <span class="ml-2">Notes</span>
+      </h1>
+      <ul class="list-disc">
+        <li>Don't get hit by meteor</li>
+      </ul>
+    </div>
+  </div>
+</template>
--- src/views/Game.vue
+++ src/views/Game.vue
@@ -21,25 +21,10 @@
         src="@/assets/logo-nar21-2.png"
       />
 
-      <div
-        class="flex justify-center items-center mt-16 p-16 rounded bg-black text-white"
-      >
-        <div>
-          <h1 class="text-xl font-bold">Instructions</h1>
-          <ul class="list-disc">
-            <li>W/A/S/D/Arrow Keys (← → ↑ ↓): move</li>
-            <li>SHIFT: slow down</li>
-          </ul>
-
-          <h1 class="text-xl font-bold">Notes</h1>
-          <ul class="list-disc">
-            <li>Don't get hit by meteor</li>
-          </ul>
-        </div>
-      </div>
+      <dialog-game-instruction />
     </div>
 
-    <choose-input-system-dialog
+    <dialog-choose-input-system
       v-if="openChooseInputSystemModal"
       @close-dialog="openChooseInputSystemModal = false"
       @play-game="playGame()"
@@ -47,9 +32,9 @@
   </section>
 
   <div class="w-screen h-screen relative overflow-hidden">
-    <canvas id="game" class="absolute" ref="bulletsCanvas" />
-    <canvas id="game" class="absolute" ref="enemiesCanvas" />
-    <canvas id="game" class="absolute" ref="playerCanvas" />
+    <canvas class="absolute" ref="bulletsCanvas" />
+    <canvas class="absolute" ref="enemiesCanvas" />
+    <canvas class="absolute" ref="playerCanvas" />
     <div id="game-background" ref="backgroundImage" />
   </div>
 </template>
@@ -57,14 +42,15 @@
 <script lang="ts">
 import { computed, defineComponent, onMounted, onUnmounted, ref } from "vue";
 
-import ChooseInputSystemDialog from "@/components/ChooseInputSystemDialog.vue";
+import DialogChooseInputSystem from "@/components/DialogChooseInputSystem.vue";
+import DialogGameInstruction from "@/components/DialogGameInstruction.vue";
 
 import Game from "@/classes/game";
 import InputSystem from "@/classes/core/input-system";
 import store from "@/store";
 
 export default defineComponent({
-  components: { ChooseInputSystemDialog },
+  components: { DialogChooseInputSystem, DialogGameInstruction },
 
   setup() {
     const backgroundImage = ref<HTMLImageElement | null>(null);
