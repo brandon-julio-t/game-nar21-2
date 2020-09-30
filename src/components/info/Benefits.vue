18513f310a1e3c8610765886512d6189567bc060
lionelritchie29 <lionelrtchieee@gmail.com>
2020-09-30 10:35:02 +0700

    tab system

---
+++ src/components/info/Benefits.vue
@@ -0,0 +1,15 @@
+<template>
+  <h1 class="text-3xl text-center">Benefits</h1>
+  <h2>Hard Skill</h2>
+  <div></div>
+
+  <h2>Soft Skill</h2>
+  <div></div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
--- src/views/About.vue
+++ src/views/About.vue
@@ -2,23 +2,43 @@
   <article
     class="flex justify-center items-center w-screen h-screen text-white"
   >
-
-    <section>
-      <div class="bg-black bg-opacity-75 p-16 rounded-lg">
-      <h1 class="text-6xl text-center">Benefits</h1>
-
-      <ul class="grid grid-cols-3 my-8">
-        <li>Hard Skill</li>
-        <li>Soft Skill</li>
-        <li>Job Experience</li>
-      </ul>
-
-      <router-link to="/" class="block text-center underline w-full"
-        >Home</router-link
-      >
+    <div class="bg-black bg-opacity-75 p-16 w-3/4 rounded-lg flex">
+      <div class="w-1/4">
+        <tabs @changetab="changeTab" />
+      </div>
+      <section class="w-3/4 ml-4">
+        <component v-bind:is="currentTabComponent"></component>
+      </section>
     </div>
-    </section>
-
-    
   </article>
 </template>
+
+<script>
+import Tabs from "../components/info/Tabs";
+import Benefits from "../components/info/Benefits";
+import Requirements from "../components/info/Requirements";
+import Test from "../components/info/Test";
+import Registration from "../components/info/Registration";
+import Contact from "../components/info/Contact";
+
+export default {
+  data() {
+    return {
+      currentStep: 1,
+      totalSteps: 5,
+      currentTab: "Benefits",
+    };
+  },
+  components: { Tabs, Benefits, Requirements, Test, Registration, Contact },
+  methods: {
+    changeTab(tabName) {
+      this.currentTab = tabName;
+    },
+  },
+  computed: {
+    currentTabComponent() {
+      return this.currentTab;
+    },
+  },
+};
+</script>
