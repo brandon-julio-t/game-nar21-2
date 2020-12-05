d1fa7b807fb224088f957ae5530e6530c7cfac78
Brandon Julio Thenaro <brandon.julio.t@icloud.com>
2020-12-05 15:03:03 +0700

    house keeping and some spacing

--- src/classes/core/star.ts
+++ src/classes/core/star.ts
@@ -8,64 +8,64 @@ import {
 import CanMove from "../interfaces/can-move";
 
 export default class Star implements CanDraw, CanMove {
-                 private readonly COLOR: string;
+  private readonly COLOR: string;
 
-                 private position: Vector2;
-                 private velocity: Vector2;
-                 private opacity: number;
-                 private trail: number;
+  private position: Vector2;
+  private velocity: Vector2;
+  private opacity: number;
+  private trail: number;
 
-                 public constructor(
-                   x: number,
-                   y: number,
-                   xVelocity: number,
-                   yVelocity: number,
-                   private radius: number,
-                   private ctx: OffscreenCanvasRenderingContext2D
-                 ) {
-                   this.position = new Vector2(x, y);
-                   this.velocity = new Vector2(xVelocity, yVelocity);
-                   this.opacity = Math.random();
-                   this.COLOR = randomHexColor();
-                   this.trail = randomIntegerBetween(5, 10);
-                 }
+  public constructor(
+    x: number,
+    y: number,
+    xVelocity: number,
+    yVelocity: number,
+    private radius: number,
+    private ctx: OffscreenCanvasRenderingContext2D
+  ) {
+    this.position = new Vector2(x, y);
+    this.velocity = new Vector2(xVelocity, yVelocity);
+    this.opacity = Math.random();
+    this.COLOR = randomHexColor();
+    this.trail = randomIntegerBetween(5, 10);
+  }
 
-                 public drawSelf(ctx: OffscreenCanvasRenderingContext2D): void {
-                   const rgb = hexToRgb(this.COLOR);
-                   if (rgb !== null) {
-                     const { opacity, position, radius, trail } = this;
-                     const { red, green, blue } = rgb;
-                     const { x, y } = position;
+  public drawSelf(ctx: OffscreenCanvasRenderingContext2D): void {
+    const rgb = hexToRgb(this.COLOR);
+    if (rgb !== null) {
+      const { opacity, position, radius, trail } = this;
+      const { red, green, blue } = rgb;
+      const { x, y } = position;
 
-                     ctx.beginPath();
-                     ctx.fillStyle = `rgba(${red}, ${green}, ${blue}, ${opacity})`;
-                     ctx.shadowBlur = trail;
-                     ctx.shadowColor = `rgba(${red}, ${green}, ${blue}, ${opacity})`;
-                     ctx.arc(x, y, radius, 0, Math.PI * 2);
-                     ctx.fill();
+      ctx.beginPath();
+      ctx.fillStyle = `rgba(${red}, ${green}, ${blue}, ${opacity})`;
+      ctx.shadowBlur = trail;
+      ctx.shadowColor = `rgba(${red}, ${green}, ${blue}, ${opacity})`;
+      ctx.arc(x, y, radius, 0, Math.PI * 2);
+      ctx.fill();
 
-                     ctx.shadowBlur = 0;
-                   }
-                 }
+      ctx.shadowBlur = 0;
+    }
+  }
 
-                 public move(): void {
-                   this.position.x += this.velocity.x;
-                   this.position.y += this.velocity.y;
-                 }
+  public move(): void {
+    this.position.x += this.velocity.x;
+    this.position.y += this.velocity.y;
+  }
 
-                 public wrapIfNecessary(): void {
-                   const { height, width } = this.ctx.canvas;
-                   const { position } = this;
-                   const { x, y } = position;
+  public wrapIfNecessary(): void {
+    const { height, width } = this.ctx.canvas;
+    const { position } = this;
+    const { x, y } = position;
 
-                   const outLeft = x < 0;
-                   const outRight = x > width;
-                   const outTop = y < 0;
-                   const outBottom = y > height;
+    const outLeft = x < 0;
+    const outRight = x > width;
+    const outTop = y < 0;
+    const outBottom = y > height;
 
-                   if (outLeft) position.x = width;
-                   if (outRight) position.x = 0;
-                   if (outTop) position.y = height;
-                   if (outBottom) position.y = 0;
-                 }
-               }
+    if (outLeft) position.x = width;
+    if (outRight) position.x = 0;
+    if (outTop) position.y = height;
+    if (outBottom) position.y = 0;
+  }
+}
