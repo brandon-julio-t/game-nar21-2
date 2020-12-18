0ba2ea4f2e44a22eff8b48c88b7e4188c8f0e5fb
Brandon Julio Thenaro <brandon.julio.t@icloud.com>
2020-12-18 14:16:02 +0700

    SL19-2

--- src/classes/core/SLake.ts
+++ src/classes/core/SLake.ts
@@ -1,11 +1,10 @@
 import Point from "@/classes/core/Point";
 import Snake from "@/classes/core/Snake";
-import { randomIntegerBetween } from "@/classes/core/utilities";
 
 export default class SLake {
   private ctx: OffscreenCanvasRenderingContext2D;
 
-  private maxSnakesCount = Math.floor(innerWidth / 100);
+  private maxSnakesCount = Math.floor(innerWidth / 50); // "The more the merrier" - SL19-2
   private points: Point[][] = [];
   private snakes: Snake[] = [];
   private animationFrameId: number = -1;
@@ -37,17 +36,33 @@ export default class SLake {
       this.points.push(rowPoints);
     }
 
-    const loop = () => {
-      this.ctx.clearRect(0, 0, this.ctx.canvas.width, this.ctx.canvas.height);
-
+    // this onmousemove will be cleaned by InputSystem
+    onmousemove = e => {
       if (this.snakes.length < this.maxSnakesCount) {
-        const randRowIdx = randomIntegerBetween(0, this.points.length - 1);
-        const randColIdx = randomIntegerBetween(
-          0,
-          this.points[randRowIdx].length - 1
-        );
-        this.snakes.push(new Snake(randRowIdx, randColIdx));
+        const { clientX, clientY } = e;
+        const row = Math.round((clientY / innerHeight) * 20);
+        const col = Math.round((clientX / innerWidth) * 40);
+
+        this.snakes.push(new Snake(row + 1, col + 1)); // +1 because row & col starts from negative and ends after limit according to gap ratio
       }
+    };
+
+    const FPS = 30;
+    const interval = 1000 / FPS;
+    let lastFrameTime = Date.now();
+
+    const loop = () => {
+      const now = Date.now();
+      const delta = now - lastFrameTime;
+
+      if (delta <= interval) {
+        this.animationFrameId = requestAnimationFrame(loop);
+        return;
+      }
+
+      lastFrameTime = now - (lastFrameTime % delta);
+
+      this.ctx.clearRect(0, 0, this.ctx.canvas.width, this.ctx.canvas.height);
 
       this.snakes.forEach((snake, idx) => {
         const currPoint = this.points[snake.rowIdx][snake.colIdx];
--- src/components/TheDialogChooseInputSystem.vue
+++ src/components/TheDialogChooseInputSystem.vue
@@ -3,9 +3,7 @@
     class="h-screen w-screen flex justify-center items-center absolute inset-0 text-white"
     style="background: #1b2f47;"
   >
-    <div
-      class="border-2 border-lionel-alternate bg-black bg-opacity-75 rounded-lg p-16 relative z-20"
-    >
+    <div class="bg-white bg-opacity-5 bg-blur rounded-3xl p-16 relative z-20">
       <h1 class="font-bold text-center mb-8">Choose Input System</h1>
 
       <div class="grid grid-cols-2 gap-2 md:gap-4 lg:gap-8">
@@ -20,7 +18,10 @@
         </app-button-main-menu>
       </div>
 
-      <button class="absolute top-0 right-0 p-4" @click="$emit('close-dialog')">
+      <button
+        class="absolute top-0 right-0 py-4 px-8"
+        @click="$emit('close-dialog')"
+      >
         <font-awesome-icon :icon="['fa', 'times']"></font-awesome-icon>
       </button>
     </div>
